5a3a7983433a4bb4b9136d84424c93008bad6e8a
Sade Christner <sadechristner@Sades-MBP.local.tld>
2015-03-23 17:55:33 -0700

    added Bootstrap and images

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,4 +1,5 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
         <script src="/assets/script.js" type="text/javascript"></script>
     </body>
 </html>
