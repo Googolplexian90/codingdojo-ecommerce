2023af1c77f1e23b2162e22e65ea11073a438978
MoneyJohne <change@me.com>
2015-03-25 18:44:31 -0700

    images

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,69 +1,62 @@
-<?php if( !defined('BASEPATH') ) exit('No direct script access is allowed.');
+<?php if (!defined('BASEPATH')) {exit('No direct script access is allowed.');
+}
 
 class Users extends CI_Controller {
-	public function __construct()
-	{
+	public function __construct() {
 		parent::__construct();
 		$this->load->model('Login');
 		$this->output->enable_profiler();
 	}
-	public function index()
-	{
-		$this->load->view('partials/header',array('title'=>'Administrator Log in'));
+	public function index() {
+		$this->load->view('partials/header', array('title' => 'Administrator Log in'));
 		$this->load->view('admin');
 		$this->load->view('partials/footer');
 	}
-	public function login()
-	{
+	public function login() {
 		$config = array(
 			array(
-				'field'=>'email',
-				'label'=>'Email',
-				'rules'=>'trim|required|valid_email'
+				'field' => 'email',
+				'label' => 'Email',
+				'rules' => 'trim|required|valid_email',
 			),
 			array(
-				'field'=>'password',
-				'label'=>'Password',
-				'rules'=>'trim|required'
+				'field' => 'password',
+				'label' => 'Password',
+				'rules' => 'trim|required',
 			)
 		);
 		$this->form_validation->set_rules($config);
-		if(!$this->form_validation->run())
-		{
-			$this->session->set_flashdata('error',validation_errors());
+		if (!$this->form_validation->run()) {
+			$this->session->set_flashdata('error', validation_errors());
 			redirect('/admin');
-		}
-		else
-		{
-			$form = $this->input->post(null,true);
+		} else {
+			$form = $this->input->post(null, true);
 			$user = $this->Login->login_user($form['email']);
-			if($user)
-			{
-				$enc = $this->encrypt($form['password'],$user['password']);
-				if($enc == $user['password'])
-				{
-					$data['id']=$user['id'];
-					$data['alias']=$user['alias'];
-					$data['email']=$user['email'];
-					$this->session->set_userdata('admin',$data);
+			if ($user) {
+				$enc = $this->encrypt($form['password'], $user['password']);
+				if ($enc == $user['password']) {
+					$data['id']    = $user['id'];
+					$data['alias'] = $user['alias'];
+					$data['email'] = $user['email'];
+					$this->session->set_userdata('admin', $data);
 					redirect('/dashboard/orders');
 				}
 			}
-			$this->session->set_flashdata('error','The email/password could not be found.');
+			$this->session->set_flashdata('error', 'The email/password could not be found.');
 			redirect('/admin');
 		}
 	}
-	public function logoff()
-	{
+	public function logoff() {
 		$u = $this->session->all_userdata();
 		$this->session->unset_userdata($u);
 		redirect('/');
 	}
 
-	protected function encrypt($password,$salt=false)
-	{
-		if(!$salt) $salt = bin2hex(openssl_random_pseudo_bytes(22));
-		return crypt($password,$salt);
+	protected function encrypt($password, $salt = false) {
+		if (!$salt) {$salt = bin2hex(openssl_random_pseudo_bytes(22));
+		}
+
+		return crypt($password, $salt);
 	}
 }
 
